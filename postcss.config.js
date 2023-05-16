451bed2ce1044f346d7cbf644693f98392f48f2f
jiy8ni <cindy030609@gmail.com>
2023-05-16 12:42:05 +0900

    수정 (3)

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer')
-      // 다른 PostCSS 플러그인들을 필요에 따라 추가할 수 있습니다.
-    ]
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   }
-  
